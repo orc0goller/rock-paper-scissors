35a38463b62aaae08acc1100f87ee675b2e17309
Gustav Drugge <druggen05@hotmail.com>
2023-07-10 21:40:02 +0200

    Create function that play one round of rock, paper & scissors

--- javascript.js
+++ javascript.js
@@ -5,12 +5,38 @@ function getComputerChoice() {
 
     switch(number) {
         case 0:
-            return "Rock";
+            return "rock";
         
         case 1:
-            return "Paper";
+            return "paper";
 
         case 2:
-            return "Scissors";
+            return "scissors";
+    }
+}
+
+// Play one round of rock, paper & scissors
+function playRound() {
+
+    // Gets player and computer choice
+    let playerSelection = prompt("What's your guess? (Rock, paper or scissors) ").toLowerCase();
+    let computerSelection = getComputerChoice();
+
+    // Returns who won
+    let win;
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Congratulations! You Win!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Congratulations! You Win!";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Congratulations! You Win!";
+    }
+    else {
+        return "You Lose!";
     }
 }
