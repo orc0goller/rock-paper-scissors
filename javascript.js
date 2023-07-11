df96605afff9a7d7602a396805f4f94c895ec43a
Gustav Drugge <druggen05@hotmail.com>
2023-07-11 11:17:08 +0200

    Create function that play 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+// Variables that keep track of wins
+let playerWins = 0;
+let computerWins = 0;
+// Declare player and computer selection
+let playerSelection;
+let computerSelection;
 
 // Returns either "Rock", "Paper" or "Scissors"
 function getComputerChoice() {
@@ -18,25 +24,51 @@ function getComputerChoice() {
 // Play one round of rock, paper & scissors
 function playRound() {
 
-    // Gets player and computer choice
-    let playerSelection = prompt("What's your guess? (Rock, paper or scissors) ").toLowerCase();
-    let computerSelection = getComputerChoice();
+    // Get player and computer choices
+    playerSelection = prompt("What's your guess? (Rock, paper or scissors) ").toLowerCase();
+    computerSelection = getComputerChoice();
 
-    // Returns who won
-    let win;
+    console.clear();
+    // Add point to winner
     if (playerSelection === computerSelection) {
-        return "It's a draw!";
+        console.log("It's a Draw!");
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "Congratulations! You Win!";
+        playerWins++;
+        console.log("You Won!");
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "Congratulations! You Win!";
+        playerWins++;
+        console.log("You Won!");
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "Congratulations! You Win!";
+        playerWins++;
+        console.log("You Won!");
     }
     else {
-        return "You Lose!";
+        computerWins++;
+        console.log("You Lost!");
     }
 }
+
+// Play rock, paper & scissors, best of 5 versus the computer
+function game() {
+    for (let round = 0; round < 5; round++) {
+        playRound();
+        console.log(`SCORE: ${playerWins}-${computerWins}`);
+    }
+
+    // Prints the winner
+    if (playerWins > computerWins) {
+        console.log(`You WIN the BO5!`);
+    }
+    else if (computerWins > playerWins) {
+        console.log(`You LOSE the BO5!`);
+    }
+    else {
+        console.log(`The BO5 is a Draw!`);
+    }
+}
+
+// Starts playing rock, paper & scissors
+game();
